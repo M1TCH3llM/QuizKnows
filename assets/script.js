66641d3a495ecb4102efd2ba9e09bb691a1c6ce5
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-08-28 18:38:42 -0600

    added highscore file, added java script for start and question, updated html.

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,70 @@
 // Timer
 
-var timeEl = document.querySelector("#timer");
+var timeSec = document.querySelector("#timer");
 
-var secondsLeft = 60;
+var start = document.querySelector("#start");
 
-function setTime() {
+var startTime = document.querySelector("#startQuiz");
+
+var score = document.querySelector("#score");
+
+startTime.addEventListener("click", function (event) {
+  var element = event.target;
+  start.textContent = "";
+
+  displayQuestion();
+  Timer();
+});
+
+var question = [
+  {
+    question: "question1",
+    choices: ["red", "green", "blue", "yellow"],
+    answer: "red",
+  },
+  {
+    question: "question2",
+    choices: ["red", "green", "blue", "yellow"],
+    answer: "blue",
+  },
+];
+
+function displayQuestion() {
+  var currentQuestion = question[0];
+  var questionContainer = document.getElementById("quiz");
+
+  var questionElement = document.createElement("h2");
+  questionElement.textContent = currentQuestion.question;
+  questionContainer.appendChild(questionElement);
+
+  var choicesList = document.createElement("ul");
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var answersJS = document.createElement("li");
+    answersJS.textContent = currentQuestion.choices[i];
+    choicesList.appendChild(answersJS);
+  }
+  questionContainer.appendChild(choicesList);
+}
+
+function Timer() {
+  var secondsLeft = 61;
   var timerInterval = setInterval(function () {
     secondsLeft--;
-     timeEl.textContent = "Times Up In " + secondsLeft;
-     
+    timeSec.textContent = "Times Up In " + secondsLeft;
+
     if (secondsLeft === 0) {
-        clearInterval(timerInterval);
-        timeEl.textContent = "Time Is Up!";
+      clearInterval(timerInterval);
+      timeSec.textContent = "Time Is Up!";
     }
   }, 1000);
 }
 
-console.log(timeEl);
+// questions
 
-setTime();
+// const quizQuestions = [
+//   {
+//     question: "who invented javaScript?",
+//     answers: ["Douglas Adams", "George Forman", "Brendon Eich", "John Lock"],
+//     correctChoice: "Brendon Eich",
+//   },
+// ];
